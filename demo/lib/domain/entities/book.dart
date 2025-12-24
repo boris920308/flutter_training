530ab76f5f994a748d5d23df9dfce7e64505f64f
boris920308 <jhoonj0308@gmail.com>
2025-12-24 18:35:51 +0900

    feat. book class 추가

---
+++ demo/lib/domain/entities/book.dart
@@ -0,0 +1,13 @@
+class Book {
+  final String title;
+  final String image;
+  final String author;
+  final String description;
+
+  Book({
+    required this.title,
+    required this.image,
+    required this.author,
+    required this.description,
+  });
+}
